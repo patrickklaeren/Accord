6faa5f6a141f340c85b9ebd8a41c5b1dd01a049a
Patrick Klaeren <pkinc@live.co.uk>
2021-06-07 21:29:16 +0100

    Migrate on startup

--- src/Accord.Web/Program.cs
+++ src/Accord.Web/Program.cs
@@ -1,13 +1,27 @@
+using System.Threading.Tasks;
+using Accord.Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Accord.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                await using var db = services.GetRequiredService<AccordContext>();
+                await db.Database.MigrateAsync();
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
