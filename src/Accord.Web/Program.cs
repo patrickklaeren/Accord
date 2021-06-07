60c8dacd564909e6d0c17483f71da06d9ce8d8ca
Patrick Klaeren <pkinc@live.co.uk>
2021-06-07 22:06:12 +0100

    Add serilog, startup logging

--- src/Accord.Web/Program.cs
+++ src/Accord.Web/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace Accord.Web
 {
@@ -11,21 +13,42 @@ namespace Accord.Web
     {
         public static async Task Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\log.txt")
+                .CreateLogger();
+
+            Log.Information("Starting up...");
+
             var host = CreateHostBuilder(args).Build();
+
+            Log.Information("Host built...");
 
             using (var scope = host.Services.CreateScope())
             {
+                Log.Information("Scope created...");
+
                 var services = scope.ServiceProvider;
 
                 await using var db = services.GetRequiredService<AccordContext>();
+
+                Log.Information("Got Db context");
+
                 await db.Database.MigrateAsync();
+
+                Log.Information("Migrated!");
             }
+
+            Log.Information("Ready to run");
 
             await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
