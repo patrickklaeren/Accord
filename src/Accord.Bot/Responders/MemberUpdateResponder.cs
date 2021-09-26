8f6012e4124c724b6f2959435f9b64018d87334a
Patrick Klaeren <pkinc@live.co.uk>
2021-09-26 12:20:49 +0100

    Fix avatar URL updating bug

--- src/Accord.Bot/Responders/MemberUpdateResponder.cs
+++ src/Accord.Bot/Responders/MemberUpdateResponder.cs
@@ -93,17 +93,17 @@ namespace Accord.Bot.Responders
 
             var avatarUrl = _discordAvatarHelper.GetAvatarUrl(user);
 
+
             await _mediator.Send(
                 new UpdateUserRequest(
                     gatewayEvent.GuildID.Value,
                     user.ID.Value,
                     user.Username,
                     user.Discriminator.ToPaddedDiscriminator(),
-                    avatarUrl,
                     gatewayEvent.Nickname.HasValue ? gatewayEvent.Nickname.Value : null,
+                    avatarUrl,
                     gatewayEvent.JoinedAt),
                 ct);
-
             return Result.FromSuccess();
         }
     }
