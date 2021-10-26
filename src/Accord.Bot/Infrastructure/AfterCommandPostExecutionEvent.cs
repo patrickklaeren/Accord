a8000768a8645092047681ef6575287199cf5ed6
Patrick Klaeren <patrick.klaeren@bspokesoftware.io>
2021-10-26 08:09:23 +0100

    Remove unmatched command message

--- src/Accord.Bot/Infrastructure/AfterCommandPostExecutionEvent.cs
+++ src/Accord.Bot/Infrastructure/AfterCommandPostExecutionEvent.cs
@@ -18,14 +18,21 @@ public class AfterCommandPostExecutionEvent : IPostExecutionEvent
         _channelApi = channelApi;
     }
 
+    private const string DEFAULT_ERROR_MESSAGE = "Something went wrong, there is no message for this, help me out by submitting a useful message via my repo!";
+    private const string NO_MATCHING_COMMAND_FOUND = "No matching command could be found.";
+
     public async Task<Result> AfterExecutionAsync(ICommandContext context, IResult commandResult, CancellationToken ct = new CancellationToken())
     {
         if (!commandResult.IsSuccess)
         {
-            var responseMessage = commandResult.Error?.Message 
-                                  ?? "Something went wrong, there is no message for this, help me out by submitting a useful message via my repo!";
-                
-            if (context is InteractionContext interactionContext)
+            var responseMessage = commandResult.Error?.Message ?? DEFAULT_ERROR_MESSAGE;
+
+            if (responseMessage == NO_MATCHING_COMMAND_FOUND)
+            {
+                // We do not care if it is not a matching command, leave it and
+                // continue
+            }
+            else if (context is InteractionContext interactionContext)
             {
                 await _interactionApi.EditOriginalInteractionResponseAsync(interactionContext.ApplicationID, interactionContext.Token, content: responseMessage, ct: ct);
             }
