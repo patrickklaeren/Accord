3533b2c690b40f8bb2a0b625f9d0cc7abda5ccf9
Patrick Klaeren <patrick.klaeren@bspokesoftware.io>
2024-11-18 20:00:02 +0000

    Add migration for .NET 9

---
+++ src/Accord.Domain/Migrations/20241118195434_DotNet9.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accord.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class DotNet9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Permissions",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Permissions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
